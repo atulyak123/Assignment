8eb1a88b4f111c327e69ec2d3ec3a42ebcb898ce
Atulya Kumari <atulya@gregorovitch.infocusp.com>
2024-12-14 18:17:51 +0530

    Assignment1

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import styles from '../src/App.module.css'
+import { ProductCard } from './Component/Card/Card'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className={styles.root}>
+        <ProductCard/>
+      
+      </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/Card/ProductDummyData.js
@@ -0,0 +1,28 @@
+ const productData=[
+    {id:1,
+      header:'Beige-Me Up, Buttercup!',
+      content:' Your Daily Dollop of Dazzlingly Delightful Neutral Chic.',
+      description:`Welcome to the wonderful world of beige—where subtlety meets sass and
+          neutral never means boring. Meet “Beige-Me Up, Buttercup,” a creamy
+          complexion savior packaged in a warm, welcoming shade that’s guaranteed
+          to make your routine feel as smooth as a spoonful of caramel mousse.
+          Spread it on with glee, knowing you’re delivering a glow that whispers,
+          “I’m effortless” while winking mischievously at the mirror. It’s time to
+          embrace the hue that never tries too hard, because sometimes being
+          gorgeously, gloriously beige is the most enchanting statement of all.`
+    },
+    {id:2,
+        header:'Tropical Delight',
+        content:' Your Daily Dollop of Dazzlingly Delightful Neutral Chic.',
+      description:`elcome to the wonderful world of beige—where subtlety meets sass and
+          neutral never means boring. Meet “Beige-Me Up, Buttercup,” a creamy
+          complexion savior packaged in a warm, welcoming shade that’s guaranteed
+          to make your routine feel as smooth as a spoonful of caramel mousse.
+          Spread it on with glee, knowing you’re delivering a glow that whispers,
+          “I’m effortless” while winking mischievously at the mirror. It’s time to
+          embrace the hue that never tries too hard, because sometimes being
+          gorgeously, gloriously beige is the most enchanting statement of all.`
+      }
+]
+export default productData
+
